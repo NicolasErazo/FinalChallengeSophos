171546e109ba77d409a4ccd895c1b7c1b5881412
Nicolas Erazo <nico.lacho@outlook.com>
2023-01-13 11:55:52 -0500

    Update Code Refactor

--- src/main/java/com/FinalChallenge/FinalChallenge/service/TransactionsServicesImplementation.java
+++ src/main/java/com/FinalChallenge/FinalChallenge/service/TransactionsServicesImplementation.java
@@ -22,57 +22,37 @@ public class TransactionsServicesImplementation implements TransactionsServices 
     @Override
     public Boolean addMovementToProduct(Transactions transaction, int id) throws ChangeSetPersister.NotFoundException {
 
-        Optional<Products> product = productsRepository.findById(id);
+        Optional<Products> productOptional = productsRepository.findById(id);
 
-        if (product.isEmpty()) {
+        if (productOptional.isEmpty()) {
             throw new ChangeSetPersister.NotFoundException();
         }
-        if (!product.get().getStatus().equals(Status.CANCELED)) {
-            Products products = product.get();
+        if (!productOptional.get().getStatus().equals(Status.CANCELED)) {
+            Products products = productOptional.get();
             transaction.setProduct_id(id);
-            transaction.setBalance(product.get().getBalance());
-            transaction.setAvailableBalance(product.get().getAvailableBalance());
+            transaction.setBalance(productOptional.get().getBalance());
+            transaction.setAvailableBalance(productOptional.get().getAvailableBalance());
 
             if (transaction.getTypeOfMovement().equals("debit")) {
-                long operation = product.get().getBalance() - transaction.getValue();
+                long operation = productOptional.get().getBalance() - transaction.getValue();
 
-                if (product.get().getAccountType().equals(AccountType.SAVINGS) && operation >= 0) {
+                if (productOptional.get().getAccountType().equals(AccountType.SAVINGS) && operation >= 0) {
                     if ((products.getAvailableBalance() - (products.getBalance() * 0.004)) >= transaction.getValue()) {
                         products.setBalance(operation);
-                        long gmf = (long) (transaction.getValue() * 0.004);
-                        if (!products.isGMF()) {
-                            if (products.getAvailableBalance() == 0) {
-                                products.setAvailableBalance(product.get().getBalance() - gmf);
-                            } else {
-                                products.setAvailableBalance(products.getAvailableBalance() - transaction.getValue() - gmf);
-                            }
-
-                        } else {
-                            products.setAvailableBalance(products.getAvailableBalance() - transaction.getValue());
-                        }
+                        addGMF(products, transaction, productOptional);
                     } else {
                         return false;
                     }
-                } else if (product.get().getAccountType().equals(AccountType.CHECKING) && operation >= -3000000) {
+                } else if (productOptional.get().getAccountType().equals(AccountType.CHECKING) && operation >= -3000000) {
                     products.setBalance(operation);
-                    long gmf = (long) (transaction.getValue() * 0.004);
-                    if (!products.isGMF()) {
-                        if (products.getAvailableBalance() == 0) {
-                            products.setAvailableBalance(product.get().getBalance() - gmf);
-                        } else {
-                            products.setAvailableBalance(products.getAvailableBalance() - transaction.getValue() - gmf);
-                        }
-
-                    } else {
-                        products.setAvailableBalance(products.getAvailableBalance() - transaction.getValue());
-                    }
+                    addGMF(products, transaction, productOptional);
 
                 } else {
                     return false;
                 }
 
-            } else if (!product.get().getStatus().equals(Status.INACTIVE)) {
-                products.setBalance(transaction.getValue() + product.get().getBalance());
+            } else if (!productOptional.get().getStatus().equals(Status.INACTIVE)) {
+                products.setBalance(transaction.getValue() + productOptional.get().getBalance());
                 products.setAvailableBalance(products.getAvailableBalance() + transaction.getValue());
             } else {
                 return false;
@@ -85,5 +65,21 @@ public class TransactionsServicesImplementation implements TransactionsServices 
         return false;
     }
 
+    private void addGMF(Products products, Transactions transaction, Optional<Products> productOptional) {
+        long gmf = (long) (transaction.getValue() * 0.004);
+        if (!products.isGMF()) {
+            if (products.getAvailableBalance() == 0) {
+                products.setAvailableBalance(productOptional.get().getBalance() - gmf);
+            } else {
+                products.setAvailableBalance(products.getAvailableBalance() - transaction.getValue() - gmf);
+            }
+
+        } else {
+            products.setAvailableBalance(products.getAvailableBalance() - transaction.getValue());
+        }
+    }
+
 }
+
+
 
